25cf2c6f3d2bbff46e1d636884228300417d9602
Xeej <55625846+Xeej@users.noreply.github.com>
2019-10-23 12:05:36 +0400

    c# winapi

---
+++ WindowsFormsApp2 LAB OS 2/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace WindowsFormsApp2_LAB_OS_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+         {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите имя файла дескриптора";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "user32";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Искать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Полное имя файла";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(284, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(149, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(107, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Handle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Идентификатор текущего процесса\r\n\r\n";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(207, 131);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(111, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Псевдодескриптоp";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(207, 160);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(111, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(287, 191);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(111, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(269, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Дескриптор, полученный функцией DuplicateHandle";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(287, 217);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(111, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(254, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Дескриптор, полученный функцией OpenProcess";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(287, 243);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(111, 20);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(287, 269);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(111, 20);
+            this.textBox9.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(86, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(186, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Номер CloseHandle DuplicateHandle";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(102, 276);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(170, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Номер CloseHandle OpenProsess";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(628, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(215, 524);
+            this.listBox1.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(625, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(162, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Все существующие процессы:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(23, 310);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(153, 251);
+            this.listBox2.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(20, 294);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(188, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Текущие потоки данного процесса:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(182, 310);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(440, 251);
+            this.listBox3.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(228, 294);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(149, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Все существующие модули:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 575);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "HANDLES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ WindowsFormsApp2 LAB OS 2/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.Diagnostics;
+
+namespace WindowsFormsApp2_LAB_OS_2
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [PreserveSig]
+        public static extern uint GetModuleFileName ([In]IntPtr hModule,[Out]StringBuilder lpFilename,[In][MarshalAs(UnmanagedType.U4)]int nSize);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetCurrentProcessId();
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetLastError();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DuplicateHandle(
+            [In] IntPtr hSourceProcessHandle,
+            [In] IntPtr hSourceHandle,
+            [In] IntPtr hTargetProcessHandle,
+            [Out] out IntPtr lpTargetHandle,
+            [In] int dwDesiredAccess,
+            [In, MarshalAs(UnmanagedType.Bool)] bool bInheritHandle,
+            [In] uint dwOptions);
+
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+         ProcessAccessFlags processAccess,
+          bool bInheritHandle,
+             int processId);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr hHandle);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)] public string szExeFile;
+        };
+
+        public struct THREADENTRY32
+
+        {
+
+            internal UInt32 dwSize;
+            internal UInt32 cntUsage;
+            internal UInt32 th32ThreadID;
+            internal UInt32 th32OwnerProcessID;
+            internal UInt32 tpBasePri;
+            internal UInt32 tpDeltaPri;
+            internal UInt32 dwFlags;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct MODULEENTRY32
+        {
+            internal uint dwSize;
+            internal uint th32ModuleID;
+            internal uint th32ProcessID;
+            internal uint GlblcntUsage;
+            internal uint ProccntUsage;
+            internal IntPtr modBaseAddr;
+            internal uint modBaseSize;
+            internal IntPtr hModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            internal string szModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            internal string szExePath;
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Thread32First(IntPtr hSnapshot, ref THREADENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Module32First(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Thread32Next(IntPtr hSnapshot, ref THREADENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Module32Next(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, uint th32ProcessID);
+
+        [Flags]
+        public enum SnapshotFlags : uint
+        {
+            HeapList = 0x00000001,
+            Process = 0x00000002,
+            Thread = 0x00000004,
+            Module = 0x00000008,
+            Module32 = 0x00000010,
+            All = (HeapList | Process | Thread | Module),
+            Inherit = 0x80000000,
+            NoHeaps = 0x40000000
+
+        }
+
+        IntPtr lpTargetHandle;
+        uint dwOptions= 0x00000002;
+
+
+        void add(ref ListBox listBox, PROCESSENTRY32 procEntry)
+        {
+            listBox.Items.Add("Размер: " + procEntry.dwSize.ToString());
+            listBox.Items.Add("ID: " + procEntry.th32ProcessID.ToString());
+            listBox.Items.Add("ID кучи: " + procEntry.th32DefaultHeapID.ToString());
+            listBox.Items.Add("Потоки: " + procEntry.cntThreads.ToString());
+            listBox.Items.Add("ID родителя: " + procEntry.th32ParentProcessID.ToString());
+            listBox.Items.Add("Стандартный приоритет: " + procEntry.pcPriClassBase.ToString());
+            listBox.Items.Add("Путь: " + procEntry.szExeFile.ToString());
+            listBox.Items.Add("");
+        }
+
+        void addth(ref ListBox listBox, THREADENTRY32 Entry)
+        {
+            listBox.Items.Add("Размер: " + Entry.dwSize.ToString());
+            listBox.Items.Add("Счетчик ссылок: " + Entry.cntUsage.ToString());
+            listBox.Items.Add("ID: " + Entry.th32ThreadID.ToString());
+            listBox.Items.Add("ID родителя: " + Entry.th32OwnerProcessID.ToString());
+            listBox.Items.Add("ID приоритет: " + Entry.tpBasePri.ToString());
+            listBox.Items.Add("");
+        }
+
+        void addMd(ref ListBox listBox, MODULEENTRY32 procEntry)
+        {
+            listBox.Items.Add("Размер: " + procEntry.dwSize.ToString());
+            listBox.Items.Add("ID: " + procEntry.th32ProcessID.ToString());
+            listBox.Items.Add("Счетчик загрузки: " + procEntry.ProccntUsage.ToString());
+            listBox.Items.Add("Базовый адрес: " + procEntry.modBaseAddr.ToString());
+            listBox.Items.Add("Размер: " + procEntry.modBaseSize.ToString());
+            listBox.Items.Add("Дескриптор: " + ((uint)procEntry.hModule).ToString());
+            listBox.Items.Add("Название модуля: " + procEntry.szModule.ToString());
+            listBox.Items.Add("Путь к модулю: " + procEntry.szExePath.ToString());
+            listBox.Items.Add("");
+    }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox4.Text = GetCurrentProcessId().ToString();
+            textBox5.Text = ((uint)GetCurrentProcess()).ToString("X");
+            DuplicateHandle(GetCurrentProcess(), GetCurrentProcess(), GetCurrentProcess(), out lpTargetHandle, 0, false, dwOptions);
+            textBox6.Text = lpTargetHandle.ToString();
+            int k = Convert.ToInt32(textBox4.Text);
+            textBox7.Text = OpenProcess(ProcessAccessFlags.All, false, k).ToString();
+            textBox8.Text = textBox6.Text;
+           // CloseHandle(OpenProcess(ProcessAccessFlags.All, false, k));
+            textBox9.Text = textBox7.Text;
+            //CloseHandle(lpTargetHandle);
+
+
+            IntPtr handleToSnapshot = IntPtr.Zero;
+            PROCESSENTRY32 procEntry = new PROCESSENTRY32();
+            procEntry.dwSize = (UInt32)Marshal.SizeOf(typeof(PROCESSENTRY32));
+            handleToSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.All, 0);
+
+            Process32First(handleToSnapshot, ref procEntry);
+            while (Process32Next(handleToSnapshot, ref procEntry))
+                add(ref listBox1, procEntry);
+
+
+            THREADENTRY32 Entry = new THREADENTRY32();
+            Entry.dwSize = (UInt32)Marshal.SizeOf(typeof(THREADENTRY32));
+            handleToSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.All, 0);
+            Thread32First(handleToSnapshot, ref Entry);
+
+            while (Thread32Next(handleToSnapshot, ref Entry))
+                if (Entry.th32OwnerProcessID == GetCurrentProcessId())
+                    addth(ref listBox2, Entry);
+
+            MODULEENTRY32 ModEntry = new MODULEENTRY32();
+            ModEntry.dwSize = (UInt32)Marshal.SizeOf(typeof(MODULEENTRY32));
+            handleToSnapshot = CreateToolhelp32Snapshot(SnapshotFlags.All, 0);
+            bool f =Module32First(handleToSnapshot, ref ModEntry);
+
+            
+            while (Module32Next(handleToSnapshot, ref ModEntry))
+                    addMd(ref listBox3, ModEntry);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr Mod = GetModuleHandle(textBox1.Text.ToString());
+
+            StringBuilder fileName = new StringBuilder(255);
+            GetModuleFileName(Mod, fileName, fileName.Capacity);
+
+            textBox3.Text = Mod.ToString("X");
+            textBox2.Text = fileName.ToString();
+        }
+
+
+    }
+}
